c8f009a037cc7bd84bd40c76a4483f495056635a
yaniksol <68951137+yaniksol@users.noreply.github.com>
2020-07-29 13:39:16 +0200

    Add files via upload

---
+++ 5. Work/1. How to find a job/index.php
@@ -0,0 +1,222 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+   
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+    <link rel="shortcut icon" type="image/x-icon" href="icon.png">
+    <title> How to find a job </title>
+      
+    <style>
+         p {
+            font-family: Arial, Helvetica, sans-serif;
+            }
+        .remove-all-margin{
+            margin:0 ! important;
+            }
+        .boja {
+            background-color: #333333;
+            }
+        .jumbotron {
+            background-image:linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)) ,url(findajob.jpg);
+            margin-top: 60px;
+            width:100%;
+            height: 580px;
+            background-size: cover;
+            }
+        #uvodniText {
+            position: relative;
+            top: 40%;
+            color: white;
+            text-align: center;
+            font-weight: bold;
+            }
+        #about {
+            background-color: #F1F1F1;
+            width: 100%;
+            position: relative;
+            }
+        #about1 {
+            background-color: #F5F5F5;
+            border-color: #F1F1F1 #6C757D #6C757D #6C757D;
+            border-style: solid;
+            border-width: 1px;
+            width: 100%;
+            padding: 55px;
+            }
+        #cards{
+            background-color: #F5F5F5;
+            width: 100%;
+            text-align: center;
+            padding: 55px;
+            position: relative;
+            margin: auto;
+            }
+        #a {
+            text-align: center;
+            }
+        .jedan {
+            color: #D46B50;
+            }
+        .dva {
+            color: #0060AD;
+            }
+        #text {
+            margin-top: 25px;
+            width: 100%;
+            }
+        #tabela {
+            margin: 20px 0px 30px 0px;
+            width: 100%;
+            }
+        
+    </style>
+      
+  </head>
+    
+    <body data-spy="scroll" data-target="#navbar" data-offset="60" >
+        
+    <nav class="navbar navbar-expand-lg navbar-dark  fixed-top " >
+        <a class="navbar-brand" href="#"><img id="logo" src="logo.png"></a>
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                     <a class="nav-link navbarText" href="http://bratgo.com"> Home </a>
+                </li>
+                
+                <li class="nav-item ">
+                    <a class="nav-link navbarText" href="http://bratgo.com/2.%20About%20the%20city/"> About the city <span class="sr-only">(current)</span></a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link navbarText" href="http://bratgo.com/3.%20Life%20in%20Bratislava/"> Starting a life in Bratislava </a>
+                </li>
+                
+                <li class="nav-item">
+                    <a class="nav-link navbarText" href="http://bratgo.com/4.%20Study/"> Study </a>
+                </li>
+                
+                <li class="nav-item">
+                    <a class="nav-link navbarText" href="http://bratgo.com/5.%20Work/"> Work </a>
+                </li>
+               
+                <li class="nav-item">
+                    <a class="nav-link navbarText" href="http://bratgo.com/6.%20Free%20time/"> What to do in a free time </a>
+                </li>
+                
+                <li class="nav-item">
+                    <a class="nav-link navbarText" href="http://bratgo.com/7.%20Need%20help/"> Need help? </a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+        
+        
+                
+        <div class="jumbotron jumbotron-fluid remove-all-margin" id="home">
+            <div class="container" id="uvodniText">
+                
+                <h1 class="display-4"> </h1>
+                                
+            </div>
+        </div>
+        
+        <div id="about">
+        <div id="about1" class="container">
+            <h1 id="a"> How to find a job </h1>
+            
+            <div id="text">
+            
+            <p class="lead"> Looking for a job is an important thing in life and a difficult one, especially if you looking for one in a foreign country.  </p>
+                
+            <p class="lead"> Don’t worry, to find a job in a Bratislava is a relatively easy thing to do. With unemployment rate less than 3%, in Bratislava and generally in the western part of Slovakia, only those who doesn’t want job, doesn’t have one.  </p>
+                
+            <p class="lead"> More difficult part of this story is how to find a job that you would like and that pays well. Luckily, there is <span class="jedan">Brat</span><span class="dva">Go</span> to help you with that. Here, you will find a list of portals where employers are regularly posting open job positions. By clicking on the name of portal, you will be redirected there.  </p>
+                
+            </div>
+        
+        <div class="card-deck container" id="cards">
+                                       
+            <div class="card bg-secondary mb-3 text-white">
+                
+                <img class="card-img-top" src="visa2.png" height="133px" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title"> Work permit</h5>
+                  <p class="card-text">
+                   <a href="http://bratgo.com/5. Work/2. Work permit" class="stretched-link"></a>
+                 </div>
+                
+            </div>
+
+                         
+          <div class="card bg-secondary mb-3 text-white">
+              
+                <img class="card-img-top" src="health.jpg" height="133px" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title"> Health insurance </h5>
+                  <p class="card-text">
+                   <a href="http://bratgo.com/5. Work/3. Health insurance" class="stretched-link"></a>
+                 </div>
+              
+           </div>
+             
+          <div class="card bg-secondary mb-3 text-white">
+              
+                <img class="card-img-top" src="iom3.jpg" height="133px" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title"> Migration center </h5>
+                  <p class="card-text">
+                   <a href="http://bratgo.com/5. Work/4. Migration center" class="stretched-link"></a>
+                </div>
+              
+            </div>
+             
+        </div>
+        </div>
+        </div>
+        
+                  
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" ></script>
+      
+            
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+      
+    <script type="text/javascript">
+         
+        
+           $(window).scroll(function () {
+   
+                if ($(document).scrollTop() > 500) {
+        
+                $('nav').addClass('boja');
+        
+                } else {
+        
+                $('nav').removeClass('boja');
+        
+                }
+    
+            });
+          
+          $('[data-toggle="popover"]').popover()
+          $('[data-toggle="tooltip"]').tooltip()
+         
+     </script>  
+  </body>
+</html>
---
+++ 6. Free time/4. Sightseeing/index.php
@@ -0,0 +1,279 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+   
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+    <link rel="shortcut icon" type="image/x-icon" href="icon.png">
+    <title> Sightseeing </title>
+      
+    <style>
+         p {
+            font-family: Arial, Helvetica, sans-serif;
+            }
+        .remove-all-margin{
+            margin:0 ! important;
+            }
+        .boja {
+            background-color: #333333;
+            }
+        .jumbotron {
+            background-image:linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)) ,url(sightseeing.jpg);
+            background-size: cover;
+            margin-top: 60px;
+            width:100%;
+            height: 580px;        
+            }
+        #uvodniText {
+            position: relative;
+            top: 40%;
+            color: white;
+            text-align: center;
+            font-weight: bold;
+            }
+        #about {
+            background-color: #F1F1F1;
+            width: 100%;
+            text-align: center;
+            position: relative;
+            }
+        #about1 {
+            background-color: #F5F5F5;
+            border-color: #F1F1F1 #6C757D #6C757D #6C757D;
+            border-style: solid;
+            border-width: 1px;
+            width: 100%;
+            text-align: center;
+            padding: 55px;
+            }
+        #cards{
+            background-color: #F5F5F5;
+            width: 75%;
+            text-align: center;
+            position: relative;
+            margin: auto;
+            margin-top: 50px;
+            }
+        #cards1{
+            background-color: #F5F5F5;
+            width: 100%;
+            text-align: center;
+            position: relative;
+            margin: auto;
+            margin-top: 20px;
+            }
+        .none {
+            visibility: hidden; 
+            }
+        
+    </style>
+      
+  </head>
+    
+    <body data-spy="scroll" data-target="#navbar" data-offset="60" >
+        
+     <nav class="navbar navbar-expand-lg navbar-dark  fixed-top " >
+        <a class="navbar-brand" href="#"><img id="logo" src="logo.png"></a>
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link navbarText" href="http://bratgo.com"> Home </a>
+                </li>
+                
+                <li class="nav-item ">
+                    <a class="nav-link navbarText" href="http://bratgo.com/2.%20About%20the%20city/"> About the city <span class="sr-only">(current)</span></a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link navbarText" href="http://bratgo.com/3.%20Life%20in%20Bratislava/"> Starting a life in Bratislava </a>
+                </li>
+                
+                <li class="nav-item">
+                    <a class="nav-link navbarText" href="http://bratgo.com/4.%20Study/"> Study </a>
+                </li>
+                
+                <li class="nav-item">
+                    <a class="nav-link navbarText" href="http://bratgo.com/5.%20Work/"> Work </a>
+                </li>
+               
+                <li class="nav-item">
+                    <a class="nav-link navbarText" href="http://bratgo.com/6.%20Free%20time/"> What to do in a free time </a>
+                </li>
+                
+                <li class="nav-item">
+                    <a class="nav-link navbarText" href="http://bratgo.com/7.%20Need%20help/"> Need help? </a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+        
+        
+                
+        <div class="jumbotron jumbotron-fluid remove-all-margin" id="home">
+            <div class="container" id="uvodniText">
+                
+                <h1 class="display-4">  </h1>
+                                
+            </div>
+        </div>
+        
+    <div id="about">
+        <div id="about1" class="container">
+            <h1> sightseeing</h1>
+            
+            <p class="lead">
+                
+                sightseeing
+                
+                 
+                    Lorem ipsum dolor sit amet, ut consulatu neglegentur mea, no sea dicam corpora. Sed tota ipsum assum te, vel quem tation quaeque in. Solet officiis accusamus no ius. Eam ei enim graecis, omnis persius at sit. Te harum suavitate vim, vim ut luptatum quaestio deterruisset. Idque nostrum te his, ea nominavi menandri mel, inermis sapientem at nam.
+
+                Argumentum persequeris ius eu, te sententiae reprehendunt sea, eos ea lorem oportere. Nobis electram ea has, duo ex duis errem apeirian. Novum antiopam intellegam pri ut, nam an quis docendi adolescens, an sed utinam fuisset accumsan. An atomorum inimicus per, ad interpretaris necessitatibus mel. Ut quo veri ipsum vitae, platonem scriptorem no eum. Tale sumo docendi mea ad, ea pro alii scripta.
+
+                Nam sensibus antiopam hendrerit no. No saepe commune pri, has in ignota integre, option alterum quaestio at sea. Invidunt adversarium no eum. Cum elitr comprehensam ne, vis an blandit imperdiet vituperatoribus. Oporteat maiestatis definiebas ius ex, ea eam erat liber impedit, ei vim eripuit fierent. Oblique efficiendi efficiantur nam et, tamquam dolorum repudiare ex vis, pri id voluptua atomorum. Quo eu iudico option, omittam molestiae sadipscing an nam, vel ad nonumes tacimates.
+
+                Vel te vide scripta meliore. Simul nonumy vis ut, an persius detraxit suscipiantur has, cu detracto inimicus expetenda cum. Sea an aperiri sanctus, accusata quaerendum definitiones at est. Mollis prompta ea his. Eum at graeci labore elaboraret, vel autem feugiat tacimates an, mel ex verear praesent.
+
+                
+            </p>
+            
+        
+        
+          <div class="card-deck container" id="cards">
+             
+            <div class="card bg-secondary mb-3 text-white">
+                
+                <img class="card-img-top" src="bar.jpg" height="133px" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title"> Bars </h5>
+                  <p class="card-text">
+                   <a href="http://bratgo.com/6.%20Free%20time/1. Bars" class="stretched-link"></a>
+                </div>
+                
+            </div>
+                           
+            <div class="card bg-secondary mb-3 text-white">
+                
+                <img class="card-img-top" src="park2.jpg" height="133px" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">  Parks </h5>
+                  <p class="card-text">
+                   <a href="http://bratgo.com/6.%20Free%20time/2. Parks" class="stretched-link"></a>
+                 </div>
+                
+            </div>
+
+                         
+          <div class="card bg-secondary mb-3 text-white">
+              
+                <img class="card-img-top" src="gym2.jpg" height="133px" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title"> Street workout and Gym </h5>
+                  <p class="card-text">
+                   <a href="http://bratgo.com/6.%20Free%20time/3. Street workout" class="stretched-link"></a>
+                 </div>
+              
+           </div>
+             
+              
+        
+            </div>
+            
+            
+            <div class="card-deck container" id="cards1">
+                
+                 <div class="card bg-secondary mb-3 text-white none">
+
+                        <img class="card-img-top" src="" height="133px" alt="Card image cap">
+                        <div class="card-body">
+                          <h5 class="card-title">  </h5>
+                          <p class="card-text">
+                           <a href="" class="stretched-link"></a>
+                        </div>
+
+                    </div>
+        
+          <div class="card bg-secondary mb-3 text-white ">
+              
+                <img class="card-img-top" src="museum2.jpg" height="133px" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title"> Museum and theatres </h5>
+                  <p class="card-text">
+                   <a href="http://bratgo.com/6.%20Free%20time/5. Museums and theatres" class="stretched-link"></a>
+                 </div>
+              
+           </div>
+             
+          <div class="card bg-secondary mb-3 text-white">
+              
+                <img class="card-img-top" src="mall2.jpg" height="133px" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title"> Malls </h5>
+                  <p class="card-text">
+                   <a href="http://bratgo.com/6.%20Free%20time/6. Mall" class="stretched-link"></a>
+                </div>
+              
+            </div>
+            
+                 <div class="card bg-secondary mb-3 text-white none">
+
+                        <img class="card-img-top" src="" height="133px" alt="Card image cap">
+                        <div class="card-body">
+                          <h5 class="card-title">  </h5>
+                          <p class="card-text">
+                           <a href="" class="stretched-link"></a>
+                        </div>
+
+                    </div>
+                      
+        </div>
+            
+         </div>
+                       
+    </div>
+        
+                  
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" ></script>
+      
+            
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+      
+    <script type="text/javascript">
+         
+        $(window).scroll(function () {
+   
+                if ($(document).scrollTop() > 500) {
+        
+                $('nav').addClass('boja');
+        
+                } else {
+        
+                $('nav').removeClass('boja');
+        
+                }
+    
+            });
+         
+          
+          $('[data-toggle="popover"]').popover()
+          $('[data-toggle="tooltip"]').tooltip()
+         
+     </script>  
+  </body>
+</html>
